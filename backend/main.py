3b56fb3f5b7c766f12c1949825f0e0d38aa6a7cb
fperez028 <21010185+fperez028@users.noreply.github.com>
2025-08-11 11:49:01 -0400

    update to backend main.py to handle CORS block

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.resume_router import router as resume_router
 from api.eval_router import router as eval_router
 from api.auth_router import router as auth_router
@@ -7,6 +9,20 @@ from api.results_router import router as results_router
 
 
 app = FastAPI()
+
+# CORS Allow React dev server
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # for dev; tighten later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # FastAPI Routes
 app.include_router(resume_router)
